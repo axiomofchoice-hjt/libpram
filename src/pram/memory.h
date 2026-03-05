7dd5de92e038a44218fab2d5e4ade3c30967204a
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-03-05 22:26:13 +0800

    refactor: enhance ReadAwaitable and WriteAwaitable structures for improved request handling

--- src/pram/memory.h
+++ src/pram/memory.h
@@ -12,18 +12,36 @@
 #include "model.h"
 
 namespace pram {
+namespace impl {
+template <typename T>
+struct ReadRequest {
+    T* internal_ref;
+    T* external_ref;
+};
+
+template <typename T>
+struct WriteRequest {
+    T* internal_ref;
+    T value;
+};
+}  // namespace impl
+
 template <typename T>
 struct ReadAwaitable {
+    std::vector<impl::ReadRequest<T>>* read_requests;
+    T* internal_ref;
     T value;
     bool await_ready() const noexcept { return false; }
-    bool await_suspend([[maybe_unused]] std::coroutine_handle<> _) noexcept { return true; }
+    void await_suspend([[maybe_unused]] std::coroutine_handle<> _) noexcept {
+        read_requests->push_back({internal_ref, &value});
+    }
     T await_resume() noexcept { return value; }
 };
 
 template <typename T>
 struct WriteAwaitable {
     bool await_ready() const noexcept { return false; }
-    bool await_suspend([[maybe_unused]] std::coroutine_handle<> _) noexcept { return true; }
+    void await_suspend([[maybe_unused]] std::coroutine_handle<> _) noexcept {}
     void await_resume() noexcept {}
 };
 
@@ -34,25 +52,25 @@ struct Memory {
 
 namespace impl {
 template <typename T>
-struct WriteRequest {
-    T* address;
-    T value;
-};
-
-namespace {
-template <typename T>
-void check_exclusive_read(const std::vector<T*>& read_requests) {
+void check_exclusive_read(const std::vector<ReadRequest<T>>& read_requests) {
     for (size_t i = 0; i + 1 < read_requests.size(); i++) {
-        if (read_requests[i] == read_requests[i + 1]) {
+        if (read_requests[i].internal_ref == read_requests[i + 1].internal_ref) {
             assert_or_throw(false, "Read conflict: exclusive read to the same address");
         }
     }
 }
 
 template <typename T>
+void apply_read(const std::vector<ReadRequest<T>>& read_requests) {
+    for (const auto& req : read_requests) {
+        *req.external_ref = *req.internal_ref;
+    }
+}
+
+template <typename T>
 void check_exclusive_write(const std::vector<WriteRequest<T>>& write_requests) {
     for (size_t i = 0; i + 1 < write_requests.size(); i++) {
-        if (write_requests[i].address == write_requests[i + 1].address) {
+        if (write_requests[i].internal_ref == write_requests[i + 1].internal_ref) {
             assert_or_throw(false, "Write conflict: exclusive write to the same address");
         }
     }
@@ -61,7 +79,7 @@ void check_exclusive_write(const std::vector<WriteRequest<T>>& write_requests) {
 template <typename T>
 void check_common_write(const std::vector<WriteRequest<T>>& write_requests) {
     for (size_t i = 0; i + 1 < write_requests.size(); i++) {
-        if (write_requests[i].address == write_requests[i + 1].address &&
+        if (write_requests[i].internal_ref == write_requests[i + 1].internal_ref &&
             write_requests[i].value != write_requests[i + 1].value) {
             assert_or_throw(false, "Write conflict: common write with different values");
         }
@@ -71,21 +89,21 @@ void check_common_write(const std::vector<WriteRequest<T>>& write_requests) {
 template <typename T>
 void apply_exclusive_common_write(const std::vector<WriteRequest<T>>& write_requests) {
     for (const auto& req : write_requests) {
-        *req.address = req.value;
+        *req.internal_ref = req.value;
     }
 }
 
 template <typename T>
 void apply_combining_write(const std::vector<WriteRequest<T>>& write_requests, const auto& combine_function) {
     for (size_t i = 0; i < write_requests.size(); i++) {
-        if (i == 0 || write_requests[i].address != write_requests[i - 1].address) {
-            *write_requests[i].address = write_requests[i].value;
+        if (i == 0 || write_requests[i].internal_ref != write_requests[i - 1].internal_ref) {
+            *write_requests[i].internal_ref = write_requests[i].value;
         } else {
-            *write_requests[i].address = combine_function(*write_requests[i].address, write_requests[i].value);
+            *write_requests[i].internal_ref =
+                combine_function(*write_requests[i].internal_ref, write_requests[i].value);
         }
     }
 }
-}  // namespace
 }  // namespace impl
 
 template <typename T>
@@ -93,7 +111,7 @@ struct SharedArray : Memory {
     std::vector<T> data;
     Model model;
 
-    std::vector<T*> read_requests;
+    std::vector<impl::ReadRequest<T>> read_requests;
     std::vector<impl::WriteRequest<T>> write_requests;
 
     SharedArray(size_t length, Model model) : data(std::vector<T>(length)), model(model) {}
@@ -101,8 +119,10 @@ struct SharedArray : Memory {
     SharedArray(std::vector<T> data, Model model) : data(std::move(data)), model(model) {}
 
     ReadAwaitable<T> read(size_t index) {
-        read_requests.push_back(&data[index]);
-        return ReadAwaitable<T>{data[index]};
+        ReadAwaitable<T> a;
+        a.internal_ref = &data[index];
+        a.read_requests = &read_requests;
+        return a;
     }
 
     WriteAwaitable<T> write(size_t index, T value) {
@@ -112,19 +132,30 @@ struct SharedArray : Memory {
 
     void commit() override {
         std::println("commit");
-        std::ranges::sort(read_requests);
-        std::ranges::sort(write_requests,
-            [](const impl::WriteRequest<T>& a, const impl::WriteRequest<T>& b) { return a.address < b.address; });
+        std::ranges::sort(read_requests, [](const impl::ReadRequest<T>& a, const impl::ReadRequest<T>& b) {
+            return a.internal_ref < b.internal_ref;
+        });
+        std::ranges::sort(write_requests, [](const impl::WriteRequest<T>& a, const impl::WriteRequest<T>& b) {
+            return a.internal_ref < b.internal_ref;
+        });
 
         if (model.read_policy == impl::ReadPolicy::Exclusive) {  // 处理互斥读
             impl::check_exclusive_read(read_requests);
+            impl::apply_read(read_requests);
         }
+        if (model.read_policy == impl::ReadPolicy::Concurrent) {  // 处理并发读
+            impl::apply_read(read_requests);
+        }
+
         if (model.write_policy == impl::WritePolicy::Exclusive) {  // 处理互斥写
             impl::check_exclusive_write(write_requests);
             impl::apply_exclusive_common_write(write_requests);
         }
         if (model.write_policy == impl::WritePolicy::Common) {  // 处理公共写
             impl::check_common_write(write_requests);
+            impl::apply_exclusive_common_write(write_requests);
+        }
+        if (model.write_policy == impl::WritePolicy::Arbitrary) {  // 处理任意写
             impl::apply_exclusive_common_write(write_requests);
         }
         if (model.write_policy == impl::WritePolicy::Add) {  // 处理合并写
