e87373a3386c2a66f757af188f480615294707c2
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-03-10 22:54:45 +0800

    add PRAMSIM_STANDALONE macro

--- include/pramsim/pramsim.hpp
+++ include/pramsim/pramsim.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#ifndef PRAMSIM_STANDALONE
 #include <pramsim/version.hpp>  // IWYU pragma: export
+#endif
 
 #include "base/assert.hpp"      // IWYU pragma: export
 #include "machine/machine.hpp"  // IWYU pragma: export
